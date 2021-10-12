44c833af238a337625b8c047714481f8455807cb
Ercüment Laçın <ercument.lacin@akinon.com>
2021-10-12 10:04:24 +0300

    Created helpers and added setcookie function with gettcookie

---
+++ src/helpers/index.js
@@ -0,0 +1,17 @@
+export function setCookie(cName, cValue, expDays) {
+  const date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${cName}=${cValue}; ${expires}; path=/`;
+}
+
+export function getCookie(cName) {
+  const name = `${cName}=`;
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArr = cDecoded.split('; ');
+  let res;
+  cArr.forEach((val) => {
+    if (val.indexOf(name) === 0) res = val.substring(name.length);
+  });
+  return res;
+}
