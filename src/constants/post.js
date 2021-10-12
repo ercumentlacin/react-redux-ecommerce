a8237b0bcb29289e765391552362fb81cef9716a
Ercüment Laçın <ercument.lacin@akinon.com>
2021-10-12 10:03:18 +0300

    unnecessary files deleted

--- src/constants/post.js
+++
@@ -1,3 +0,0 @@
-export const FETCH_POST = 'FETCH_POST';
-export const FETCHED_POST = 'FETCHED_POST';
-export const RECEIVE_ERROR = 'RECEIVE_ERROR';
--- src/constants/user.js
+++
@@ -1,3 +0,0 @@
-export const FETCH_USER = 'FETCH_USER';
-export const FETCHED_USER = 'FETCHED_USER';
-export const RECEIVE_ERROR = 'RECEIVE_ERROR';
