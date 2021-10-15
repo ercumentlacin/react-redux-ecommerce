72ec65a3e0a99f942df8fcae26aea7ac1defda15
Ercüment Laçın <ercument.lacin@akinon.com>
2021-10-15 16:49:35 +0300

    cart page redesigned

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,11 +1,70 @@
-import BasketWrapper from './scBasket';
+import { getCartAction } from 'actions/cart';
+import Spinner from 'components/Spinner/Spinner';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import BasketSc from './scBasket';
 
 const Basket = () => {
   console.log('hello :>> ', 'basket page');
+  // eslint-disable-next-line no-unused-vars
+  const { cart, error, pending, user } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted && !cart.length) {
+      dispatch(getCartAction());
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, [cart.length, dispatch]);
+
+  const renderBasketItems = () =>
+    cart.map((item) => (
+      <BasketSc.Item key={item.id}>
+        <div className="left">
+          <img src={item.image} alt={item.name} />
+        </div>
+
+        <BasketSc.Right>
+          <div className="top">
+            <h3>{item.name}</h3>
+            <p>${item.price}</p>
+          </div>
+
+          <BasketSc.Bottom>
+            <div className="left">
+              <button type="button">
+                <i className="fas fa-minus" />
+              </button>
+              <p>{item.quantity}</p>
+              <button type="button">
+                <i className="fas fa-plus" />
+              </button>
+            </div>
+
+            <div className="trash">
+              <button type="button">
+                <i className="fas fa-trash-alt" />
+              </button>
+            </div>
+          </BasketSc.Bottom>
+        </BasketSc.Right>
+      </BasketSc.Item>
+    ));
+
+  if (pending) return <Spinner />;
+
   return (
-    <BasketWrapper>
-      <h1>basket</h1>
-    </BasketWrapper>
+    <BasketSc.Wrapper>
+      <h1>My Cart</h1>
+
+      <div>{renderBasketItems()}</div>
+    </BasketSc.Wrapper>
   );
 };
 
--- src/pages/Basket/scBasket.js
+++ src/pages/Basket/scBasket.js
@@ -1,7 +1,102 @@
 import styled from 'styled-components';
 
-const BasketWrapper = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  h1 {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+  }
 `;
-export default BasketWrapper;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+
+  .left {
+    border-radius: 10px;
+    img {
+      width: 100px;
+      height: 100px;
+      margin-right: 10px;
+    }
+  }
+
+  .right {
+  }
+`;
+
+const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 100px;
+
+  .top {
+    h3 {
+      margin-bottom: 5px;
+      font-size: 1.1rem;
+    }
+    p {
+      font-size: 0.9rem;
+      color: hsl(204 86% 86% / 70%);
+    }
+  }
+
+  .bottom {
+  }
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  user-select: none;
+
+  button {
+    all: unset;
+    border: 1px solid hsl(204 86% 86% / 100%);
+    color: hsl(204 86% 86% / 100%);
+    background: none;
+    cursor: pointer;
+    border-radius: 50%;
+    width: 1.5rem;
+    height: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s ease-in-out;
+
+    i {
+      font-size: 13px;
+    }
+
+    &:hover {
+      background: hsl(204 86% 86% / 100%);
+      color: #1b262c;
+    }
+  }
+
+  .left {
+    display: flex;
+    align-items: center;
+    font-size: 0.9rem;
+    color: hsl(204 86% 86% / 70%);
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+
+    p {
+      margin: 0 10px;
+    }
+  }
+`;
+
+export default {
+  Bottom,
+  Wrapper,
+  Item,
+  Right,
+};
